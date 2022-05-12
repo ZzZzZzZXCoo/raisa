b04c997dd3b6307c41da3d36f05ffd661b243c6f
ZzZzZzZXCoo <77143312+ZzZzZzZXCoo@users.noreply.github.com>
2022-05-12 17:43:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 cd /usr/bin
-wget -q "https://raw.githubusercontent.com/ZzZzZzZXCoo/raisa/main/raisa"
+wget -q "https://raw.githubusercontent.com/ZzZzZzZXCoo/raisa/main/vpnjav"
 wget -q -O menu "https://raw.githubusercontent.com/ZzZzZzZXCoo/raisa/main/menu.sh"
 wget -q -O ssh "https://raw.githubusercontent.com/ZzZzZzZXCoo/raisa/main/ssh.sh"
-wget -q -O wgr "https://raw.githubusercontent.com/ZzZzZzZXCoo/raisa/main/wgr.sh"
-wget -q -O l2tp "https://raw.githubusercontent.com/ZzZzZzZXCoo/raisa/main/l2tp.sh"
-wget -q -O sstpp "https://raw.githubusercontent.com/ZzZzZzZXCoo/raisa/main/sstpp.sh"
 wget -q -O ssssr "https://raw.githubusercontent.com/ZzZzZzZXCoo/raisa/main/ssssr.sh"
-wget -q -O v2raay "https://raw.githubusercontent.com/ZzZzZzZXCoo/raisa/main/v2raay.sh"
-wget -q -O vleess "https://raw.githubusercontent.com/ZzZzZzZXCoo/raisa/main/vleess.sh"
-wget -q -O trojaan "https://raw.githubusercontent.com/ZzZzZzZXCoo/raisa/main/trojaan.sh"
+wget -q -O xraay "https://raw.githubusercontent.com/ZzZzZzZXCoo/raisa/main/v2raay.sh"
 chmod +x menu && sed -i -e 's/\r$//' menu
 chmod +x ssh &&  sed -i -e 's/\r$//' ssh
-chmod +x wgr && sed -i -e 's/\r$//' wgr
-chmod +x l2tp && sed -i -e 's/\r$//' l2tp
-chmod +x sstpp && sed -i -e 's/\r$//' sstpp
 chmod +x ssssr && sed -i -e 's/\r$//' ssssr
-chmod +x v2raay &&  sed -i -e 's/\r$//' v2raay
-chmod +x vleess &&  sed -i -e 's/\r$//' vleess
-chmod +x trojaan &&  sed -i -e 's/\r$//' trojaan
+chmod +x xraay &&  sed -i -e 's/\r$//' xraay
